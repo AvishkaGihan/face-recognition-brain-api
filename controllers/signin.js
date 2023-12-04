5ab621856e3d99386c96507800b8c4b488e68d86
AvishkaGihan <avishkag18@gmail.com>
2023-12-04 17:29:34 +0530

    Fix form validation in register and signin controllers

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,9 +1,13 @@
 const handleSignin = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
   db.select("email", "hash")
-    .where("email", "=", req.body.email)
+    .where("email", "=", email)
     .from("login")
     .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db
           .select("*")
