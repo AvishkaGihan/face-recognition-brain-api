5b4530bea7e985f14a1521bbd78ebf53111f3160
AvishkaGihan <avishkag18@gmail.com>
2023-12-05 00:33:37 +0530

    Add SSL configuration to database connection

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
 const db = knex({
   client: "pg",
   connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     host: process.env.DATABASE_HOST,
     port: 5432,
     user: process.env.DATABASE_USER,
